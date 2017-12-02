1bae196c9f765aea8b195b271e723a03e90018d4
Shiva Prasad <mailshiva19@gmail.com>
2017-12-03 02:35:03 +0530

    using local aar file

--- src/tensorflow.common.ts
+++ src/tensorflow.common.ts
@@ -1,28 +1,33 @@
-import { Observable } from 'tns-core-modules/data/observable';
-import * as app from 'tns-core-modules/application';
-import * as dialogs from 'tns-core-modules/ui/dialogs';
-
+import { Observable } from "tns-core-modules/data/observable";
+import * as app from "tns-core-modules/application";
+import * as dialogs from "tns-core-modules/ui/dialogs";
+declare var org: any;
 export class Common extends Observable {
-  public message: string;
+    public message: string;
 
-  constructor() {
-    super();
-    this.message = Utils.SUCCESS_MSG();
-  }
+    constructor() {
+        super();
+        this.message = Utils.SUCCESS_MSG();
+        console.log(org.tensorflow);
+    }
 
-  public greet() {
-    return "Hello, NS";
-  }
+    public greet() {
+        return "Hello, NS";
+    }
 }
 
 export class Utils {
-  public static SUCCESS_MSG(): string {
-    let msg = `Your plugin is working on ${app.android ? 'Android' : 'iOS'}.`;
+    public static SUCCESS_MSG(): string {
+        let msg = `Your plugin is working on ${
+            app.android ? "Android" : "iOS"
+        }.`;
 
-    setTimeout(() => {
-      dialogs.alert(`${msg} For real. It's really working :)`).then(() => console.log(`Dialog closed.`));
-    }, 2000);
+        setTimeout(() => {
+            dialogs
+                .alert(`${msg} For real. It's really working :)`)
+                .then(() => console.log(`Dialog closed.`));
+        }, 2000);
 
-    return msg;
-  }
+        return msg;
+    }
 }
